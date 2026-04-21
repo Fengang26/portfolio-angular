468f189799c6b99c7d33799fd94e30a5dff34878
elisee fengang <elisee.fengang@codexsoft.dev>
2026-04-21 13:22:55 +0100

    Initial commit

---
+++ src/app/shared/components/buttonview/buttonview.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buttonview',
+  imports: [],
+  templateUrl: './buttonview.html',
+  styleUrl: './buttonview.scss',
+})
+export class Buttonview {
+
+}
