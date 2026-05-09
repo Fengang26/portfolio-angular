332783ae58d7dde1958aab157c81713cec4b43ea
elisee fengang <elisee.fengang@codexsoft.dev>
2026-05-09 10:42:27 +0100

    Remove server-side rendering and unused components and update dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeuix/themes/aura';
-
+import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
-    providePrimeNG({
-      theme: {
-        preset: Aura
-      }
-    })
+    provideRouter(routes, withViewTransitions()),
+    provideAnimationsAsync(),
   ]
 };
